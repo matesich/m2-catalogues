e458a4504e0b3eb2eb186ee5f5ab7415f8f613b6
Mate Sitchinava <mate.sitchinava@scandiweb.com>
2022-09-11 02:54:46 +0400

    Added a new product with a migration patch

---
+++ app/code/Scandiweb/Test/Setup/Patch/Data/CreateShrekToyProduct.php
@@ -0,0 +1,236 @@
+<?php
+
+namespace Scandiweb\Test\Setup\Patch\Data;
+
+use Exception;
+use Magento\Catalog\Api\CategoryLinkManagementInterface;
+use Magento\Catalog\Api\Data\ProductInterfaceFactory;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Catalog\Model\Product\Type;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\State;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\StateException;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Validation\ValidationException;
+use Magento\InventoryApi\Api\Data\SourceItemInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Scandiweb\Test\Helper\MediaMigration;
+
+class CreateShrekToyProduct implements DataPatchInterface
+{
+    const MIGRATION_MODULE = 'Scandiweb_Test';
+
+    const MEDIA_PATH = 'catalog/product';
+
+    /**
+     * @var State
+     */
+    protected State $appState;
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    protected ModuleDataSetupInterface $setup;
+
+    /**
+     * @var ProductInterfaceFactory
+     */
+    protected ProductInterfaceFactory $productInterfaceFactory;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    protected ProductRepositoryInterface $productRepository;
+
+    /**
+     * @var EavSetup
+     */
+    protected EavSetup $eavSetup;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected StoreManagerInterface $storeManager;
+
+    /**
+     * @var SourceItemInterfaceFactory
+     */
+    protected SourceItemInterfaceFactory $sourceItemFactory;
+
+    /**
+     * @var SourceItemsSaveInterface
+     */
+    protected SourceItemsSaveInterface $sourceItemsSaveInterface;
+
+    /**
+     * @var CategoryLinkManagementInterface
+     */
+    protected CategoryLinkManagementInterface $categoryLink;
+
+    /**
+     * @var array
+     */
+    protected array $sourceItems = [];
+
+    /**
+     * @var CategoryCollectionFactory
+     */
+    protected CategoryCollectionFactory $categoryCollectionFactory;
+
+    /**
+     * @var MediaMigration
+     */
+    protected MediaMigration $mediaHelper;
+
+    /**
+     * @var DirectoryList
+     */
+    protected DirectoryList $directoryList;
+
+    /**
+     * Migration patch constructor.
+     *
+     * @param State $appState
+     * @param ModuleDataSetupInterface $setup
+     * @param ProductInterfaceFactory $productInterfaceFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param StoreManagerInterface $storeManager
+     * @param EavSetup $eavSetup
+     * @param SourceItemInterfaceFactory $sourceItemFactory
+     * @param SourceItemsSaveInterface $sourceItemsSaveInterface
+     * @param CategoryLinkManagementInterface $categoryLink
+     * @param CategoryCollectionFactory $categoryCollectionFactory
+     * @param MediaMigration $mediaHelper
+     * @param DirectoryList $directoryList
+     */
+    public function __construct(
+        State                           $appState,
+        ModuleDataSetupInterface        $setup,
+        ProductInterfaceFactory         $productInterfaceFactory,
+        ProductRepositoryInterface      $productRepository,
+        StoreManagerInterface           $storeManager,
+        EavSetup                        $eavSetup,
+        SourceItemInterfaceFactory      $sourceItemFactory,
+        SourceItemsSaveInterface        $sourceItemsSaveInterface,
+        CategoryLinkManagementInterface $categoryLink,
+        CategoryCollectionFactory       $categoryCollectionFactory,
+        MediaMigration                  $mediaHelper,
+        DirectoryList                   $directoryList
+    ) {
+        $this->appState = $appState;
+        $this->productInterfaceFactory = $productInterfaceFactory;
+        $this->productRepository = $productRepository;
+        $this->setup = $setup;
+        $this->eavSetup = $eavSetup;
+        $this->storeManager = $storeManager;
+        $this->sourceItemFactory = $sourceItemFactory;
+        $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
+        $this->categoryLink = $categoryLink;
+        $this->categoryCollectionFactory = $categoryCollectionFactory;
+        $this->mediaHelper = $mediaHelper;
+        $this->directoryList = $directoryList;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function apply()
+    {
+        $this->appState->emulateAreaCode('adminhtml', [$this, 'execute']);
+
+        return $this;
+    }
+
+    /**
+     * @throws CouldNotSaveException
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     * @throws ValidationException
+     * @throws StateException
+     * @throws InputException
+     */
+    public function execute()
+    {
+        $files = [
+            'shrek.jpg'
+        ];
+
+        $this->mediaHelper->copyMediaFiles($files, self::MIGRATION_MODULE, self::MEDIA_PATH);
+
+        $product = $this->productInterfaceFactory->create();
+
+        if ($product->getIdBySku('shrek-toy')) {
+            return;
+        }
+
+        $attributeSetId = $this->eavSetup->getAttributeSetId(Product::ENTITY, 'Default');
+
+        $product->setTypeId(Type::TYPE_SIMPLE)
+            ->setAttributeSetId($attributeSetId)
+            ->setName('Shrek Ogre 15" Decent Quality Plush Soft Stuffed Animal Doll')
+            ->setSku('shrek-toy')
+            ->setUrlKey('shrektoy')
+            ->setPrice(99.99)
+            ->setVisibilty(Visibility::VISIBILITY_BOTH)
+            ->setStatus(Status::STATUS_ENABLED);
+
+        $mediaUrl = $this->directoryList->getPath('media') . '/' . self:: MEDIA_PATH . '/';
+        $imagePath = 'shrek.jpg';
+        $product->addImageToMediaGallery($mediaUrl . $imagePath, ['image', 'small_image', 'thumbnail'], false, false);
+
+        $product->setCustomAttribute('Material', 'Plush');
+
+        $websiteIds = [$this->storeManager->getStore()->getWebsiteId()];
+
+        $product->setWebsiteIds($websiteIds);
+
+        $product->setStockData(
+            [
+                'use_config_manage_stock' => 1,
+                'is_qty_decimal' => 0,
+                'is_in_stock' => 1
+            ]
+        );
+
+        $product = $this->productRepository->save($product);
+
+        $sourceItem = $this->sourceItemFactory->create();
+        $sourceItem->setSourceCode('default');
+        $sourceItem->setQuantity(100);
+        $sourceItem->setSku($product->getSku());
+        $sourceItem->setStatus(SourceItemInterface::STATUS_IN_STOCK);
+        $this->sourceItems[] = $sourceItem;
+
+        $this->sourceItemsSaveInterface->execute($this->sourceItems);
+
+        $this->categoryLink->assignProductToCategories($product->getSku(), [2]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+}
